5aa183b039f7f5699537a12118542324f0c7ab19
yangaimin <13103825148@136.com>
2019-07-17 21:00:47 +0800

    第一次上传项目Springboot框架

---
+++ src/main/java/com/xcy/WanlitiaoyiApplication.java
@@ -0,0 +1,13 @@
+package com.xcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanlitiaoyiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanlitiaoyiApplication.class, args);
+    }
+
+}
