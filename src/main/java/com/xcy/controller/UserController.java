7bc2d1e0e478124e09a5363e3d397c2df66a3bc3
yangaimin <13103825148@136.com>
2019-07-18 12:48:30 +0800

    忘记密码验证发送二维码

--- src/main/java/com/xcy/controller/UserController.java
+++ src/main/java/com/xcy/controller/UserController.java
@@ -5,6 +5,7 @@ import com.xcy.service.UserService;
 import com.xcy.utils.EmailYzmUtils;
 import com.xcy.utils.MailUtils;
 import com.xcy.utils.Md5Util;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +30,6 @@ public class UserController {
     @RequestMapping("validEmail")
     @ApiOperation("检查email是否存在，如果不存在，发送验证码，返回值0代表邮箱已被注册，1代表发送成功,-1代表发送验证码失败")
     public String validEmail(String email, HttpServletRequest request){
-        System.out.println(email+"++++++++++++++++++++++++++++++++++++++++++");
         int result = userService.validEmail(email);
         if (result > 0){
             return "0";//说明该邮箱已被注册
@@ -45,6 +45,26 @@ public class UserController {
 
         }
     }
+
+
+    @RequestMapping("forgetPassword")
+    @ApiModelProperty("忘记密码，检查email是否存在，如果不存在，返回-1，存在则发送验证码成功,返回1，发送验证码失败返回0")
+    public int forgetPasword(String email,HttpServletRequest request){
+        int result = userService.validEmail(email);
+        if(result > 0){
+            String yzm = EmailYzmUtils.getYzm();
+            boolean flag = MailUtils.sendMail(email, "你好，你现在正在进行邂逅之恋的密码找回，验证码为" + yzm + "，15分钟内有效。", "邂逅之恋");
+            if (flag){
+                request.getSession().setAttribute("registerYzm",yzm);
+                return 1;//发送验证码成功
+            } else {
+                return 0;//发送验证码失败
+            }
+        } else {
+            return -1;//该账户不存在
+        }
+    }
+
 
     @RequestMapping("register")
     @ApiOperation("注册功能,需要传入的值为邮箱和密码，注册成功返回该用户ID，注册失败返回-1")
